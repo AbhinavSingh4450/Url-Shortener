4f5087c8561c06e47d99f8857e2f205306f22842
Abhinav Singh <sabhinavpratap2@gmail.com>
2025-08-07 00:01:05 +0530

    added deployed backend url in frontend

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000", 
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:5000"
+      : "https://url-shortener-kc4b.onrender.com",
 });
